823b28add73bdf373086154af3b970997f6ecd0b
Huy <minhhuy8137@gmail.com>
2020-06-14 22:11:48 +0200

    11

--- src/layouts/MainHeader.vue
+++ src/layouts/MainHeader.vue
@@ -1,11 +1,11 @@
 <template>
-<div>
-  <div class="gt-sm">
-    <q-layout view="hHh LpR lfr">
+  <q-layout view="hHh LpR lfr">
+    <div v-if="windowWidth > 600">
       <q-header elevated class="row justify-center text-grey bg-white">
         <q-toolbar>
           <q-toolbar-title class="title">
-            Quizzi<span style="color: #F2994A">O</span>n 2.0
+            Quizzi
+            <span style="color: #F2994A">O</span>n 2.0
           </q-toolbar-title>
 
           <q-tabs
@@ -33,27 +33,21 @@
           </q-tabs>
         </q-toolbar>
       </q-header>
+    </div>
 
-      <q-page-container>
-        <router-view />
-      </q-page-container>
-    </q-layout>
-  </div>
-
-  <div class="lt-sm">
-    <q-layout view="hHh LpR lfr">
+    <div v-else>
       <q-header elevated class="row justify-center text-grey bg-white">
         <q-toolbar>
           <q-toolbar-title class="title text-center">
-            Quizzi<span style="color: #F2994A">O</span>n 2.0
+            Quizzi
+            <span style="color: #F2994A">O</span>n 2.0
           </q-toolbar-title>
-          </q-toolbar>
-          <q-toolbar >
+        </q-toolbar>
+        <q-toolbar>
           <q-tabs
             class="text-#828282; q-tabs-phone"
             active-color="primary"
             indicator-color="primary"
-
             v-if="typeof this.$store.state.authLogin == 'undefined' || this.$store.state.authLogin.token == null"
           >
             <q-route-tab to="join" label="Join" />
@@ -72,53 +66,12 @@
           </q-tabs>
         </q-toolbar>
       </q-header>
+    </div>
 
-      <q-page-container>
-        <router-view />
-      </q-page-container>
-    </q-layout>
-  </div>
-
-  <div class="sm">
-    <q-layout view="hHh LpR lfr">
-      <q-header elevated class="row justify-center text-grey bg-white">
-        <q-toolbar>
-          <q-toolbar-title class="title">
-            Quizzi<span style="color: #F2994A">O</span>n 2.0
-          </q-toolbar-title>
-
-          <q-tabs
-            shrink
-            stretch
-            class="text-#828282"
-            active-color="primary"
-            indicator-color="primary"
-            v-if="typeof this.$store.state.authLogin == 'undefined' || this.$store.state.authLogin.token == null"
-          >
-            <q-route-tab to="join" label="Join" />
-            <q-route-tab to="signin" label="Sign in" />
-          </q-tabs>
-          <q-tabs
-            shrink
-            stretch
-            class="text-#828282"
-            active-color="primary"
-            indicator-color="primary"
-            v-else
-          >
-            <q-route-tab to="dashboard" label="Dashboard" />
-            <q-route-tab to="profile" label="Profile" />
-            <q-route-tab to="logout" label="Log out" />
-          </q-tabs>
-        </q-toolbar>
-      </q-header>
-
-      <q-page-container>
-        <router-view />
-      </q-page-container>
-    </q-layout>
-  </div>
-</div>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
@@ -129,10 +82,28 @@ export default {
 
   data() {
     return {
-      loggedIn: this.$store.state.authLogin.token == null || typeof this.$store.state.authLogin == 'undefined' ? false : true
+      loggedIn:
+        this.$store.state.authLogin.token == null ||
+        typeof this.$store.state.authLogin == "undefined"
+          ? false
+          : true,
+      windowWidth: 0
+    };
+  },
+
+  mounted() {
+    this.$nextTick(function() {
+      window.addEventListener("resize", this.getWindowWidth);
+      this.getWindowWidth();
+    });
+  },
+
+  methods: {
+    getWindowWidth(event) {
+      this.windowWidth = document.documentElement.clientWidth;
     }
   }
-}
+};
 </script>
 <style lang="scss" scoped>
 .title {
