13414f50c6b8a9528dce2e04923015e8139e2157
Huy <minhhuy8137@gmail.com>
2020-06-14 17:45:50 +0200

    9

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const
   express = require('express'),
   serveStatic = require('serve-static'),
   history = require('connect-history-api-fallback'),
-  port = process.env.PORT || 5000,
-  path = require('path')
+  port = process.env.PORT || 5000
 
 const app = express()
 
 app.use(history())
-app.use(serveStatic(path.join(__dirname, '/dist/spa')))
-app.listen(port)
+
+app.use(serveStatic(__dirname + '/dist/spa'))
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
