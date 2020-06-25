db5ec59f0352a3617d9c15e87fb3e7b420643f46
Huy <minhhuy8137@gmail.com>
2020-06-25 15:00:18 +0200

    1

--- src/components/InvitationCodeInput.vue
+++ src/components/InvitationCodeInput.vue
@@ -86,7 +86,7 @@ export default {
     ...mapGetters("authLogin", ["token"])
   },
   mounted() {
-    this.socket = io("https://3.212.180.89", {
+    this.socket = io("http://3.212.180.89", {
       autoConnect: false,
       transport: ['websocket']
     });
