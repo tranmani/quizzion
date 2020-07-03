2ac374272a9775f4475f6029e192e03b7a6e1642
Huy <minhhuy8137@gmail.com>
2020-07-03 20:02:08 +0200

    1

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,6 @@
   
   <div id="q-app">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-36117294-9"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-36117294-9');
-</script>
-
     <router-view />
   </div>
 </template>
